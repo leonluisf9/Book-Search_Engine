6cf72f0a261180cf84fe357821f654a9623b1952
Maverick <leonluisf9@outlook.com>
2025-05-04 15:33:37 -0500

    updated cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -27,7 +27,7 @@ async function startServer() {
   console.log("Apollo Server Started!");
 
   app.use((_req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://book-search-engine-7c4s.onrender.com");
+    res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
